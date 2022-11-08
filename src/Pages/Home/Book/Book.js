a66ad31840626b16f1c3c8cd053e2ddaf0bd86c3
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-08 07:18:21 +0600

    some changes added in cards

--- src/Pages/Home/Book/Book.js
+++ src/Pages/Home/Book/Book.js
@@ -12,8 +12,9 @@ const Book = ({ book }) => {
             </Link>
             <div className="px-5 pb-5">
                 <Link to="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{ bookName }</h5>
-                    <p>Author: { authorName }</p>
+                    <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">{ bookName }</h5>
+                    <p className='text-lg font-semibold'>Author: { authorName }</p>
+                    <p>{ bookDetails.slice(0, 50) }...</p>
                 </Link>
                 <div className="flex items-center mt-2.5 mb-5">
                     <svg aria-hidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -25,7 +26,7 @@ const Book = ({ book }) => {
                 </div>
                 <div className="flex justify-between items-center">
                     <span className="text-3xl font-bold text-gray-900 dark:text-white">{ price }</span>
-                    <Link to="#" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Add to cart</Link>
+                    <Link to="#" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">View Details</Link>
                 </div>
             </div>
         </div>
